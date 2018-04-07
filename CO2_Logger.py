ff46b5c4b28f8e08db3c3610e150758e0a692410
Semack2015 <fan26@mail.ru>
2018-04-07 19:09:14 +0800

    Попытка создания файла в зависимости от даты

--- CO2_Logger.py
+++ CO2_Logger.py
@@ -3,7 +3,6 @@ import re
 import serial.tools.list_ports
 
 config = open('config.txt')
-log = open('Port_Log.txt', 'w')
 
 
 port = config.read().split('\n')[0]
@@ -19,9 +18,8 @@ time.sleep(2)
 ser = serial.Serial(port, 9600, timeout=1)
 
 while(1):
-    #d = 0
-    a1 = 0
-    a2 = 0
+    # d = 0
+    a1 = -1
     ser.write(b'=')
     a = ser.readline()
     a = str(a)
@@ -31,37 +29,42 @@ while(1):
     # a = a.replace('\\', '')
     # a = a.replace('r', '')
     # a = a.replace('n', '')
-    if a == 'b\'BREAK\\r\\n\'' :
-        a = re.sub(r'b\'BREAK\\n\\r\'', 'BREAK', a)
-        #d = 1
+    if a == 'b\'BREAK\\r\\n\'':
+        a = "BREAK"
+        # d = 1
     else:
         if a == 'b\'CheckSum failed\\r\\n\'' :
-            a = re.sub(r'b\'CheckSum failed\\n\\r\'', 'CheckSum failed', a)
-            #d = 1
+            a = "CheckSum failed"
+            # d = 1
         else:
-            a = re.sub('[^0123456789\s]', "", a)
-            if a.split(' ').__len__() == 2:
-                if a.split(' ')[0] != '':
-                    a1 = int(a.split(' ')[0])
-                if a.split(' ')[1] != '':
-                    a2 = int(a.split(' ')[1])
+            if a == 'b\'Preheat\\r\\n\'':
+                a = "Preheat"
+            else:
+                a = re.sub('[^0123456789]', "", a)
+            # if a.split(' ').__len__() == 2:
+            #     if a.split(' ')[0] != '':
+            #         a1 = int(a.split(' ')[0])
+            #     if a.split(' ')[1] != '':
+            #         a2 = int(a.split(' ')[1])
 
-    a3 = a1 * 256 + a2
+    # a3 = a1 * 256 + a2
 
-    if a3 == 0:
-        print(time.strftime("%d.%m.%Y %H:%M:%S"), '\t', a)
+    if a1 == -1:
+        print(time.strftime("%d.%m.%Y\t%H:%M:%S"), '\t', a)
         file = open('Port_Log.txt', 'a')
-        file.write(time.strftime("%d.%m.%Y %H:%M:%S"))
+        file.write(time.strftime("%d.%m.%Y\t%H:%M:%S"))
         file.write('\t')
         file.write(str(a))
         file.write('\n')
         file.close()
     else:
-        print(time.strftime("%d.%m.%Y %H:%M:%S"), '\t', a3)
-        file = open('CO2_Log.txt', 'a')
-        file.write(time.strftime("%d.%m.%Y %H:%M:%S"))
+        print(time.strftime("%d.%m.%Y\t%H:%M:%S"), '\t', a1)
+        # date = time.strftime("%d.%m.%Y CO2_Log.txt")
+        date = 'CO2_Logger.txt'
+        file = open(date, 'a')
+        file.write(time.strftime("%d.%m.%Y\t%H:%M:%S"))
         file.write('\t')
-        file.write(str(a3))
+        file.write(str(a1))
         file.write('\n')
         file.close()
 
